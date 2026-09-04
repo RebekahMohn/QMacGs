4be7f06b8be006c3ea14f8dc11932b210b75ff76
RebekahMohn <mohnx072@umn.edu>
2026-09-04 14:03:00 -0500

    Updating file structure and adding missing files to Admixture

--- 05_PREPROCESSING/02b_master_trim_script.bash
+++ 05_PREPROCESSING/02b_master_trim_script.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -e slurm_%A_%a.err
 #SBATCH -o slurm_%A_%a.out 
-#SBATCH --array=681-693
+#SBATCH --array=1-693
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=3G
 #SBATCH --mail-type=ALL
