d93875f3a9af85ceebcb12a457f0cba7c4eaca83
RebekahMohn <mohnx072@umn.edu>
2026-09-04 11:42:26 -0500

    updating the array range

--- 10_ToMONGOLICA/01_VCF/02b_master_map_script.bash
+++ 10_ToMONGOLICA/01_VCF/02b_master_map_script.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -e slurm_map_%A_%a.err
 #SBATCH -o slurm_map_%A_%a.out 
-#SBATCH --array=1-72
+#SBATCH --array=1-721
 #SBATCH -c 20
 #SBATCH --mem-per-cpu=4G
 #SBATCH --mail-type=ALL
